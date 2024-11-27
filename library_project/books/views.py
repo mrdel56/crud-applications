befde2a338b6494766f6434ddd576b8a4fad985b
MR_DEL <del118241@gmail.com>
2024-11-27 13:52:25 +0600

    initialized crud applications

---
+++ library_project/books/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Book
+from .forms import BookForm
+
+# List all books
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+# Create a new book
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'book_form.html', {'form': form})
+
+# Update an existing book
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'book_form.html', {'form': form})
+
+# Delete a book
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'book_confirm_delete.html', {'book': book})
